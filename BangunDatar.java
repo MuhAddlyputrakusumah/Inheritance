dafc7662dffc41e57bebc4d4212b391cd056c23e
MuhAddlyputrakusumah <143059519+MuhAddlyputrakusumah@users.noreply.github.com>
2024-01-26 14:15:10 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADDLY
+ */
+public class BangunDatar {
+    float luas(){
+        System.out.println("Menghitung laus bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    } 
+}
---
+++ Segitiga.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADDLY
+ */
+public class Segitiga extends BangunDatar {
+    
+    float alas;
+    float tinggi;
+  @Override
+    float luas() {
+        float luas = 1/2 * (alas * tinggi);
+        System.out.println("Luas Segitiga: " + luas);
+        return luas;
+    }   
+    
+  
+    
+}
